afe7a3174a79eac5a155aca56a2f22cc030385a9
nebrosarth <68643510+nebrosarth@users.noreply.github.com>
2024-03-12 07:11:04 +0500

    plots

--- Raster-Map-data-and-code/code/U_Net_D.py
+++ Raster-Map-data-and-code/code/U_Net_D.py
@@ -1,8 +1,6 @@
 import matplotlib.pyplot as plt
 from keras.losses import binary_crossentropy
 
-
-
 plt.style.use("ggplot")
 
 from tqdm import tqdm_notebook
@@ -22,13 +20,13 @@ from keras.models import Model
 from tensorflow.keras.layers import Input, concatenate, Conv2D, MaxPooling2D, Conv2DTranspose
 from tensorflow.keras.layers import Activation, add, multiply, Lambda
 from tensorflow.keras.layers import UpSampling2D, Dropout
-from tensorflow.keras.optimizers import Adam,SGD
+from tensorflow.keras.optimizers import Adam, SGD
 from keras.callbacks import ModelCheckpoint
 from tensorflow.keras import backend as K
 from keras.layers import Flatten
 from sklearn.model_selection import train_test_split
 
-#tf.debugging.set_log_device_placement(True)
+# tf.debugging.set_log_device_placement(True)
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 gpus = tf.config.experimental.list_physical_devices(device_type='GPU')
 
@@ -45,57 +43,65 @@ def AttnGatingBlock(x, g, inter_shape, name):
     then, upsample g to be same size as x 
     add x and g (concat_xg)
     relu, 1x1 conv, then sigmoid then upsample the final - this gives us attn coefficients'''
-    
+
     shape_x = K.int_shape(x)  # 32
     shape_g = K.int_shape(g)  # 16
 
-    theta_x = Conv2D(inter_shape, (2, 2), strides=(2, 2), padding='same', name='xl'+name)(x)  # 16
+    theta_x = Conv2D(inter_shape, (2, 2), strides=(2, 2), padding='same', name='xl' + name)(x)  # 16
     shape_theta_x = K.int_shape(theta_x)
 
     phi_g = Conv2D(inter_shape, (1, 1), padding='same')(g)
-    upsample_g = Conv2DTranspose(inter_shape, (3, 3),strides=(shape_theta_x[1] // shape_g[1], shape_theta_x[2] // shape_g[2]),padding='same', name='g_up'+name)(phi_g)  # 16
+    upsample_g = Conv2DTranspose(inter_shape, (3, 3),
+                                 strides=(shape_theta_x[1] // shape_g[1], shape_theta_x[2] // shape_g[2]),
+                                 padding='same', name='g_up' + name)(phi_g)  # 16
 
     concat_xg = add([upsample_g, theta_x])
     act_xg = Activation('relu')(concat_xg)
-    psi = Conv2D(1, (1, 1), padding='same', name='psi'+name)(act_xg)
+    psi = Conv2D(1, (1, 1), padding='same', name='psi' + name)(act_xg)
     sigmoid_xg = Activation('sigmoid')(psi)
     shape_sigmoid = K.int_shape(sigmoid_xg)
     upsample_psi = UpSampling2D(size=(shape_x[1] // shape_sigmoid[1], shape_x[2] // shape_sigmoid[2]))(sigmoid_xg)  # 32
 
-    upsample_psi = expend_as(upsample_psi, shape_x[3],  name)
-    y = multiply([upsample_psi, x], name='q_attn'+name)
+    upsample_psi = expend_as(upsample_psi, shape_x[3], name)
+    y = multiply([upsample_psi, x], name='q_attn' + name)
 
-    result = Conv2D(shape_x[3], (1, 1), padding='same',name='q_attn_conv'+name)(y)
-    result_bn = BatchNormalization(name='q_attn_bn'+name)(result)
+    result = Conv2D(shape_x[3], (1, 1), padding='same', name='q_attn_conv' + name)(y)
+    result_bn = BatchNormalization(name='q_attn_bn' + name)(result)
     return result_bn
+
 
 def UnetGatingSignal(input, is_batchnorm, name):
     ''' this is simply 1x1 convolution, bn, activation '''
     shape = K.int_shape(input)
-    x = Conv2D(shape[3] * 1, (1, 1), strides=(1, 1), padding="same",  name=name + '_conv')(input)
+    x = Conv2D(shape[3] * 1, (1, 1), strides=(1, 1), padding="same", name=name + '_conv')(input)
     if is_batchnorm:
         x = BatchNormalization(name=name + '_bn')(x)
-    x = Activation('relu', name = name + '_act')(x)
+    x = Activation('relu', name=name + '_act')(x)
     return x
 
+
 def UnetConv2D(input, outdim, is_batchnorm, name):
-	x = Conv2D(outdim, (3, 3), strides=(1, 1), kernel_initializer="he_normal",padding="same", name=name+'_1')(input)
-	if is_batchnorm:
-		x =BatchNormalization(name=name + '_1_bn')(x)
-	x = Activation('relu',name=name + '_1_act')(x)
+    x = Conv2D(outdim, (3, 3), strides=(1, 1), kernel_initializer="he_normal", padding="same", name=name + '_1')(input)
+    if is_batchnorm:
+        x = BatchNormalization(name=name + '_1_bn')(x)
+    x = Activation('relu', name=name + '_1_act')(x)
 
-	x = Conv2D(outdim, (3, 3), strides=(1, 1),  kernel_initializer="he_normal",padding="same", name=name+'_2')(x)
-	if is_batchnorm:
-		x = BatchNormalization(name=name + '_2_bn')(x)
-	x = Activation('relu', name=name + '_2_act')(x)
-	return x
+    x = Conv2D(outdim, (3, 3), strides=(1, 1), kernel_initializer="he_normal", padding="same", name=name + '_2')(x)
+    if is_batchnorm:
+        x = BatchNormalization(name=name + '_2_bn')(x)
+    x = Activation('relu', name=name + '_2_act')(x)
+    return x
 
-def expend_as(tensor, rep,name):
-	my_repeat = Lambda(lambda x, repnum: K.repeat_elements(x, repnum, axis=3), arguments={'repnum': rep},  name='psi_up'+name)(tensor)
-	return my_repeat
+
+def expend_as(tensor, rep, name):
+    my_repeat = Lambda(lambda x, repnum: K.repeat_elements(x, repnum, axis=3), arguments={'repnum': rep},
+                       name='psi_up' + name)(tensor)
+    return my_repeat
+
 
 epsilon = 1e-5
 smooth = 1
+
 
 def dsc(y_true, y_pred):
     smooth = 1.
@@ -105,84 +111,92 @@ def dsc(y_true, y_pred):
     score = (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
     return score
 
+
 def dice_loss(y_true, y_pred):
     loss = 1 - dsc(y_true, y_pred)
     return loss
+
 
 def bce_dice_loss(y_true, y_pred):
     loss = binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
     return loss
 
+
 def confusion(y_true, y_pred):
-    smooth=1
+    smooth = 1
     y_pred_pos = K.clip(y_pred, 0, 1)
     y_pred_neg = 1 - y_pred_pos
     y_pos = K.clip(y_true, 0, 1)
     y_neg = 1 - y_pos
     tp = K.sum(y_pos * y_pred_pos)
     fp = K.sum(y_neg * y_pred_pos)
-    fn = K.sum(y_pos * y_pred_neg) 
-    prec = (tp + smooth)/(tp+fp+smooth)
-    recall = (tp+smooth)/(tp+fn+smooth)
+    fn = K.sum(y_pos * y_pred_neg)
+    prec = (tp + smooth) / (tp + fp + smooth)
+    recall = (tp + smooth) / (tp + fn + smooth)
     return prec, recall
+
 
 def tp(y_true, y_pred):
     smooth = 1
     y_pred_pos = K.round(K.clip(y_pred, 0, 1))
     y_pos = K.round(K.clip(y_true, 0, 1))
-    tp = (K.sum(y_pos * y_pred_pos) + smooth)/ (K.sum(y_pos) + smooth) 
-    return tp 
+    tp = (K.sum(y_pos * y_pred_pos) + smooth) / (K.sum(y_pos) + smooth)
+    return tp
+
 
 def tn(y_true, y_pred):
     smooth = 1
     y_pred_pos = K.round(K.clip(y_pred, 0, 1))
     y_pred_neg = 1 - y_pred_pos
     y_pos = K.round(K.clip(y_true, 0, 1))
-    y_neg = 1 - y_pos 
-    tn = (K.sum(y_neg * y_pred_neg) + smooth) / (K.sum(y_neg) + smooth )
-    return tn 
+    y_neg = 1 - y_pos
+    tn = (K.sum(y_neg * y_pred_neg) + smooth) / (K.sum(y_neg) + smooth)
+    return tn
+
 
 def tversky(y_true, y_pred):
     y_true_pos = Flatten()(y_true)
     y_pred_pos = Flatten()(y_pred)
     true_pos = K.sum(y_true_pos * y_pred_pos)
-    false_neg = K.sum(y_true_pos * (1-y_pred_pos))
-    false_pos = K.sum((1-y_true_pos)*y_pred_pos)
+    false_neg = K.sum(y_true_pos * (1 - y_pred_pos))
+    false_pos = K.sum((1 - y_true_pos) * y_pred_pos)
     alpha = 0.7
-    return (true_pos + smooth)/(true_pos + alpha*false_neg + (1-alpha)*false_pos + smooth)
+    return (true_pos + smooth) / (true_pos + alpha * false_neg + (1 - alpha) * false_pos + smooth)
+
 
 def tversky_loss(y_true, y_pred):
-    return 1 - tversky(y_true,y_pred)
+    return 1 - tversky(y_true, y_pred)
 
-def focal_tversky(y_true,y_pred):
+
+def focal_tversky(y_true, y_pred):
     pt_1 = tversky(y_true, y_pred)
     gamma = 0.75
-    return K.pow((1-pt_1), gamma)
+    return K.pow((1 - pt_1), gamma)
 
-def unet(opt,input_size, lossfxn):
-  
+
+def unet(opt, input_size, lossfxn):
     inputs = Input(shape=input_size)
     conv1 = UnetConv2D(inputs, 32, is_batchnorm=True, name='conv1')
-    conv1 = Dropout(0.2,name='drop_conv1')(conv1)
+    conv1 = Dropout(0.2, name='drop_conv1')(conv1)
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-    
+
     conv2 = UnetConv2D(pool1, 64, is_batchnorm=True, name='conv2')
-    conv2 = Dropout(0.2,name='drop_conv2')(conv2)
+    conv2 = Dropout(0.2, name='drop_conv2')(conv2)
     pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-    
 
     conv3 = UnetConv2D(pool2, 128, is_batchnorm=True, name='conv3')
-    conv3 = Dropout(0.2,name='drop_conv3')(conv3)
+    conv3 = Dropout(0.2, name='drop_conv3')(conv3)
     pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
 
     conv4 = UnetConv2D(pool3, 256, is_batchnorm=True, name='conv4')
-    conv4 = Dropout(0.2,name='drop_conv4')(conv4)
+    conv4 = Dropout(0.2, name='drop_conv4')(conv4)
     pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
 
     conv5 = Conv2D(512, (3, 3), activation='relu', kernel_initializer=kinit, padding='same')(pool4)
     conv5 = Conv2D(512, (3, 3), activation='relu', kernel_initializer=kinit, padding='same')(conv5)
 
-    up6 = concatenate([Conv2DTranspose(256, (2, 2), strides=(2, 2), kernel_initializer=kinit, padding='same')(conv5), conv4], axis=3)
+    up6 = concatenate(
+        [Conv2DTranspose(256, (2, 2), strides=(2, 2), kernel_initializer=kinit, padding='same')(conv5), conv4], axis=3)
     conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(up6)
     conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv6)
 
@@ -190,15 +204,18 @@ def unet(opt,input_size, lossfxn):
     conv7 = Conv2D(128, (3, 3), activation='relu', kernel_initializer=kinit, padding='same')(up7)
     conv7 = Conv2D(128, (3, 3), activation='relu', kernel_initializer=kinit, padding='same')(conv7)
 
-    up8 = concatenate([Conv2DTranspose(64, (2, 2), strides=(2, 2), kernel_initializer=kinit, padding='same')(conv7), conv2], axis=3)
+    up8 = concatenate(
+        [Conv2DTranspose(64, (2, 2), strides=(2, 2), kernel_initializer=kinit, padding='same')(conv7), conv2], axis=3)
     conv8 = Conv2D(64, (3, 3), activation='relu', kernel_initializer=kinit, padding='same')(up8)
 
-    up9 = concatenate([Conv2DTranspose(32, (2, 2), strides=(2, 2), kernel_initializer=kinit, padding='same')(conv8), conv1], axis=3)
-    conv9 = Conv2D(32, (3, 3), activation='relu',  kernel_initializer=kinit, padding='same')(up9)
+    up9 = concatenate(
+        [Conv2DTranspose(32, (2, 2), strides=(2, 2), kernel_initializer=kinit, padding='same')(conv8), conv1], axis=3)
+    conv9 = Conv2D(32, (3, 3), activation='relu', kernel_initializer=kinit, padding='same')(up9)
     conv9 = Conv2D(32, (3, 3), activation='relu', kernel_initializer=kinit, padding='same')(conv9)
     conv10 = Conv2D(1, (1, 1), activation='sigmoid', name='final')(conv9)
 
     model = Model(inputs=[inputs], outputs=[conv10])
+
     def prec(y_true, y_pred):
         _, recall = confusion(y_true, y_pred)
         return recall
@@ -207,16 +224,17 @@ def unet(opt,input_size, lossfxn):
         prec, _ = confusion(y_true, y_pred)
         return prec
 
-    model.compile(optimizer=opt, loss=lossfxn, metrics=[dsc,tp,tn,prec,recall])
+    model.compile(optimizer=opt, loss=lossfxn, metrics=[dsc, tp, tn, prec, recall])
     return model
 
 
 im_width = 256
 im_height = 256
-path_train ='E:\\Raster-Map\\Raster-Map-data-and-code\\training_dataset\\tianditu\\'
+path_train = 'E:\\Raster-Map\\Raster-Map-data-and-code\\training_dataset\\tianditu\\'
 path_test = 'E:\\Raster-Map\\Raster-Map-data-and-code\\training_dataset\\tianditu/test/'
 
-#加载数据
+
+# 加载数据
 # Get and resize train images and masks
 def get_data(path, train=True):
     ids = next(os.walk(path + "images"))[2]
@@ -224,7 +242,7 @@ def get_data(path, train=True):
     if train:
         y = np.zeros((len(ids), im_height, im_width, 1), dtype=np.float32)
     print('Getting and resizing images ... ')
-    for n, id_ in tqdm_notebook(enumerate(ids), total=len(ids),disable=True):
+    for n, id_ in tqdm_notebook(enumerate(ids), total=len(ids), disable=True):
         # Load images
         img = load_img(path + 'images/' + id_)
         x_img = img_to_array(img)
@@ -250,11 +268,9 @@ def get_data(path, train=True):
     else:
         return X
 
+    # X, y = get_data(path_train, train=True)
 
-X, y = get_data(path_train, train=True)
-
-
-X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.3, random_state=2019)
+    # X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.3, random_state=2019)
 
 img_row = 256
 img_col = 256
@@ -262,27 +278,53 @@ img_chan = 1
 epochnum = 50
 batchnum = 8
 smooth = 1.
-    
+
+
 sgd = SGD(learning_rate=0.01, momentum=0.90, decay=1e-6)
 adam = Adam(learning_rate=1e-3)
-
+#
 input_size = (img_row, img_col, img_chan)
-K.set_image_data_format('channels_last')  # TF dimension ordering in this code
+# K.set_image_data_format('channels_last')  # TF dimension ordering in this code
 kinit = 'he_normal'
 
 with tf.device('/GPU:0'):
     model = unet(adam, input_size, dice_loss)
 
-#训练网络模型U-net
+# 训练网络模型U-net
 callbacks = [
-#     EarlyStopping(patience=10, verbose=1),
+    #     EarlyStopping(patience=10, verbose=1),
     ReduceLROnPlateau(factor=0.1, patience=3, min_lr=0.000001, verbose=1),
-    ModelCheckpoint("/Raster-Map-data-and-code/model/model-T_unet-maproad-fc2d.weights.h5", verbose=1, save_best_only=True, save_weights_only=True)
+    ModelCheckpoint("E:\\Raster-Map-data-and-code/model/model-T_unet-maproad-fc2d.weights.h5", verbose=1,
+                    save_best_only=True, save_weights_only=True)
 ]
-#u-net模型训练
-results = model.fit(X_train, y_train, batch_size=1, epochs=100, callbacks=callbacks, validation_data=(X_valid, y_valid))
+# u-net模型训练
+# results = model.fit(X_train, y_train, batch_size=1, epochs=100, callbacks=callbacks, validation_data=(X_valid, y_valid))
 X_test, y_test = get_data(path_test, train=True)
-preds_train = model.predict(X_train, verbose=1)
-preds_val = model.predict(X_valid, verbose=1)
-preds_test = model.predict(X_test, verbose=1)
+# preds_train = model.predict(X_train, verbose=1)
+# preds_val = model.predict(X_valid, verbose=1)
+# preds_test = model.predict(X_test, verbose=1)
+# model.evaluate(X_test, y_test, batch_size=8, verbose=1)
+
+print(os.path.dirname(__file__))
+a = os.path.dirname(__file__)
+# Load model from file
+model_path = f"E:/Raster-Map/Raster-Map-data-and-code/model/model-T_unet-maproad-fc2d.weights.h5"
+model.load_weights(model_path)
+# Test model on test data
+
 model.evaluate(X_test, y_test, batch_size=8, verbose=1)
+preds_test = model.predict(X_test, verbose=1)
+
+print(preds_test)
+
+# Создаем фигуру с 40 подграфиками в сетке 5x8
+fig, axes = plt.subplots(5, 8, figsize=(16, 10))
+
+# Перебираем все подграфики и рисуем на них соответствующие картинки из массива
+# Используем squeeze() для удаления лишней размерности
+for i, ax in enumerate(axes.flat):
+    ax.imshow(preds_test[i].squeeze(), cmap="gray")
+    ax.axis("off")
+
+# Показываем фигуру
+plt.show()
